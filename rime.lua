207f5b2d1ace852fdf3fc0a5cacd269586c484e4
Sun Wuan <s5unty@gmail.com>
2022-04-13 16:57:36 +0800

    弄堂

---
+++ rime.lua
@@ -0,0 +1,9 @@
+
+-- date_translator: 将 `date` 翻译为当前日期
+-- 详见 `lua/date.lua`:
+date_translator = require("date")
+
+-- single_char_filter: 候选项重排序，使单字优先
+-- 详见 `lua/single_char.lua`
+single_char_filter = require("single_char")
+
